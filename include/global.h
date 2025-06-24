e71fc791e0363ba63566c7580ad74d22d1160f31
xFoudres <xFoudres@soul.io>
2025-06-24 16:30:34 +0200

    Add blockchainAddress in SaveBlock2

--- include/global.h
+++ include/global.h
@@ -535,7 +535,9 @@ struct SaveBlock2
     /*0x57C*/ struct RankingHall2P hallRecords2P[FRONTIER_LVL_MODE_COUNT][HALL_RECORDS_COUNT]; // From record mixing.
     /*0x624*/ u16 contestLinkResults[CONTEST_CATEGORIES_COUNT][CONTESTANT_COUNT];
     /*0x64C*/ struct BattleFrontier frontier;
-}; // sizeof=0xF2C
+    /*0xF2C*/ u8 blockchainAddress[63]; // 62 bytes for address + 1 for null terminator
+		
+}; // sizeof=0xF2C NewSizeOf=0xF6B
 
 extern struct SaveBlock2 *gSaveBlock2Ptr;
 
